dd0c9fdcfb16243d005ab216f2914404d6c994e1
Mohamed Elsoudy <mohamedeloudy373@gmail.com>
2023-01-12 02:18:14 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+//get  vadieos from Youtube api 
+
+// let http =new XMLHttpRequest();
+
+// http.open("GET","https://www.googleapis.comAIzaSyAJ_l7ePVGVGmWRhg2d57wy8xnwM2mrKMs/youtube/v3/videos");
+
+// http.send();
+
+// http.addEventListener("readystatechange",()=>{
+//   if(http.readyState==200){
+//     console.log(http.response)
+//   }
+// })
